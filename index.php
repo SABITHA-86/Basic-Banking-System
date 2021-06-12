e7bef1246c4f9893461ac5656738726d082f0615
SABITHA-86 <85760323+SABITHA-86@users.noreply.github.com>
2021-06-12 11:36:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>Basic Banking System</title>
+  </head>
+<style>
+  
+h1 {
+	font-size: 52px;
+	margin-top: -7px;
+	margin-left: 700px;
+}
+h2{
+	margin-left: 845px;
+	margin-top: 40px;
+	font-size: 30px;
+}
+h4{
+	font-size: 22px;
+	margin-left: 730px;
+	letter-spacing: 1px;
+	color: #236B8E;
+	text-shadow: 0px 1px;
+}
+
+</style>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+<div class="container-fluid">
+<div class="header">
+<br>
+<h2 style="font-family:Lucida Calligraphy">Welcome To</h2>
+<h1><strong>TSF BANK</strong></h1>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
